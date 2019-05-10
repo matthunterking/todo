c5799460a5e764496db63e41b3560529f9b3c0c3
matthunterking <matthew85king@gmail.com>
2019-05-10 13:35:34 +0100

    Added loading component

---
+++ src/components/Loading/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import s from "./style.module.css";
+
+const Loading = props => {
+  return <p>Loading...</p>;
+};
+
+export default Loading;
--- src/hoc/withListItems.js
+++ src/hoc/withListItems.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Loading from "../components/Loading";
 
 const withListItems = WrappedComponent => {
   const WithListItems = props => {
@@ -25,7 +26,7 @@ const withListItems = WrappedComponent => {
       [isLoading]
     );
 
-    if (isLoading) return <p>Loading...</p>;
+    if (isLoading) return <Loading />;
     return (
       <WrappedComponent
         listItems={listItems}
