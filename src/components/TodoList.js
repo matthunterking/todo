2748a7ed368a82224a61c4be660b00bdc815d14f
matthunterking <matthew85king@gmail.com>
2019-05-10 12:24:23 +0100

    Added total number of items and number of completed items

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,16 +4,21 @@ import s from "./style.module.css";
 
 const ToDoList = props => {
   const [listItems, setListItems] = useState([]);
+  const [completedItems, setCompletedItems] = useState(null);
 
   useEffect(() => {
     axios.get("http://localhost:3000/api/todos").then(({ data }) => {
       const sortedData = data.sort((a, b) => b.id - a.id);
+      const completed = data.filter(item => item.completed);
       setListItems(sortedData);
+      setCompletedItems(completed.length);
     });
   }, []);
 
   return (
     <div>
+      <p>Total number of items {listItems.length}</p>
+      <p>Total number completed {completedItems}</p>
       {listItems.map(({ userId, id, title, completed }) => (
         <div key={id} className={s.listItem}>
           <h2>{title}</h2>
