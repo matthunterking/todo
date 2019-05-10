7de235157ba336a77dc5f51ccbe90f7680893f29
matthunterking <matthew85king@gmail.com>
2019-05-10 12:12:11 +0100

    Added request for to items to to do list component. Added cors to back end

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const data = require("./data.json");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
-app.get("/api/todos", function (_req, res) {
+app.get("/api/todos", function(_req, res) {
   res.status(200).send(data);
-})
-app.use(express.static('public'));
+});
+app.use(express.static("public"));
 
-app.listen(3000, function () {
-  console.log(`🚄 Server running @ http://localhost:3000`)
+app.listen(3000, function() {
+  console.log(`🚄 Server running @ http://localhost:3000`);
 });
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const ToDoList = props => {
-  return <p>To do list</p>;
+  const [listItems, setListItems] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/todos").then(({ data }) => {
+      console.log(data);
+      setListItems(data);
+    });
+  });
+
+  return (
+    <div>
+      {listItems.map(({ userId, id, title, completed }) => (
+        <div key={id}>
+          <span>{id}</span>
+          <span>{title}</span>
+          <span>{userId}</span>
+          <span>{completed ? "completed" : ""}</span>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ToDoList;
