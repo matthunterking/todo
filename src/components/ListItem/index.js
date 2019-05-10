4e21ea8533af1d719878ed5391af36cc9eb32dc0
matthunterking <matthew85king@gmail.com>
2019-05-10 14:13:05 +0100

    Added styling to list items

--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -1,13 +1,22 @@
 import React from "react";
 import s from "./style.module.css";
+import cn from "classnames";
 
 const ListItem = ({ id, title, userId, completed }) => {
   return (
-    <div className={s.container}>
+    <div className={cn(s.container, { [s.completed]: completed })}>
       <h2>{title}</h2>
-      <p>User : {userId}</p>
-      <p> ID : {id}</p>
-      <p>{completed ? "completed" : "not completed"}</p>
+      <div className={s.informationContainer}>
+        <p>Item Id : {id}</p>
+        <p>User Id: {userId}</p>
+      </div>
+      <div className={s.statusContainer}>
+        {completed ? (
+          <p className={cn(s.stats, s.tick)}>&#10003;</p>
+        ) : (
+          <p className={cn(s.stats, s.cross)}>&#10007;</p>
+        )}
+      </div>
     </div>
   );
 };
